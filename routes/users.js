8b1ef3714dc94682074506777801201f9c8249dc
Osman Caliskan <osmancaliskan64@gmail.com>
2019-04-19 21:05:39 -0500

    the application now connected to mongoDB database for creating new user and holding user info in db.

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var multer= require("multer");
 var upload = multer({dest:'./uploads'});
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -56,7 +58,22 @@ router.post('/register', upload.single('profileimage'), function(req, res, next)
        errors: errors
      });
    } else {
-     console.log('No Errors');
+      var newUser = new User({
+        name: name,
+        email: email,
+        username: username,
+        password: password,
+        profileimage: profileimage
+      });
+      User.createUser(newUser, function(err,user){
+        if(err) throw err;
+        console.log(user);
+      });
+      req.flash('success', 'Now you are registered and can login');
+
+
+      res.location('/');
+      res.redirect('/');
    }   
 });
 
